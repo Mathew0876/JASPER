9bf40d26b0abcb456097f2d3a564253a89a0dc6f
sammytripp <samtripp5@gmail.com>
2020-12-31 18:01:00 +0000

    Install Laravel Breeze framework

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
