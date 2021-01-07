454a49d94b4af4bdb632f06f0676e515032ab648
Britney <bakebr75@gmail.com>
2021-01-07 18:07:50 -0500

    side bar height

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,33 +1,33 @@
 <!--Sidebar-->
-<div class="bg-jasper-gray space-y-10 top-0 left-0 w-64 h-full ">
+<div class="bg-jasper-gray space-y-10 top-0 left-0 w-64 h-screen  ">
     <div class="space-y-4">
-        <div class="m-2 p-4 hover:bg-jasper-purple">
+        <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Requirements</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-jasper-purple">
+        <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Import</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-jasper-purple">
+        <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Export</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-jasper-purple">
+        <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Settings</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-jasper-purple">
+        <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Help</a> 
             </h2>
         </div>
     </div>
-    <div class="m-2 p-4">
+    <div class="p-4">
         <h2 class = "text-center">
             <a class="text-white text-sm font-sans-roboto align-middle">JASPER &copy;</a>
         </h2>
