e822a8fa3c6e08c21a91837cc21e031515c092ae
sammytripp <samtripp5@gmail.com>
2021-01-07 19:02:30 +0000

    Edit extended colours set-up

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,32 +1,32 @@
 <!--Sidebar button-->
 <!--Sidebar-->
-<div class="bg-gray-600 space-y-10 top-0 left-0 w-auto h-full absolute">
+<div class="bg-jasper-gray space-y-10 top-0 left-0 w-auto h-full absolute">
     <div class="m-2 p-4 flex items-center">
         <img class="h-10 w-15" src='../../images/Logo.PNG'>
         <h1 class="text-white text-4xl font-sans-roboto font-bold">JASPER</h1>
     </div>
     <div class="space-y-4">
-        <div class="m-2 p-4 hover:bg-purple-400">
+        <div class="m-2 p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Requirements</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-purple-400">
+        <div class="m-2 p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Import</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-purple-400">
+        <div class="m-2 p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Export</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-purple-400">
+        <div class="m-2 p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Settings</a> 
             </h2>
         </div>
-        <div class="m-2 p-4 hover:bg-purple-400">
+        <div class="m-2 p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
                 <a class="text-white text-lg font-sans-roboto align-middle">Help</a> 
             </h2>
