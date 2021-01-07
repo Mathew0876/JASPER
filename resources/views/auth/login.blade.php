47dbf8fe11bcb01086499c58f4c158843a6e253b
sammytripp <samtripp5@gmail.com>
2021-01-06 20:23:15 -0500

    Compile tailwind css, update laravel-mix and postcss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
+        
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -2,7 +2,7 @@
     <div>
         {{ $logo }}
     </div>
-
+    
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
