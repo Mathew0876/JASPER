1181e4a6a108711c3e725e18b4fe45469b1e3284
Britney <bakebr75@gmail.com>
2021-01-08 11:33:28 -0500

    Line added to sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!--Sidebar-->
-<div class="bg-jasper-gray space-y-10 top-0 left-0 w-64 h-full min-h-screen flex-none">
+<div class="flex-none bg-jasper-gray space-y-10 top-0 left-0 w-64 h-full min-h-screen">
     <div class="space-y-4">
         <div class="p-4 hover:bg-jasper-purple">
             <h2 class = "text-center">
@@ -33,7 +33,7 @@
         </div>
     </div>
     <div class="p-4">
-        <h2 class = "text-center">
+        <h2 class = "text-center w-auto border-t border-white content-center">
             <a class="text-white text-sm font-sans-roboto align-middle">JASPER &copy;</a>
         </h2>
     </div>
